066cea0f027d0d543cf601114e3fb13e3f7166b1
baskaklimek <baska.klimekova@gmail.com>
2022-10-08 08:18:06 +0200

    simplier implementation

---
+++ Assesment_Blood_pressure_easier/app/src/main/java/com/mcma/blood_pressure_diary/BloodPressureReading.java
@@ -0,0 +1,48 @@
+package com.mcma.blood_pressure_diary;
+
+/**
+ * This class shall be used to contain a reading of
+ * systolic and diastolic blood pressure readings, as
+ * well as the corresponding timestamp when the measurement
+ * was taken
+ *
+ * Please extend this class with necessary functions (e.g.
+ * constructor and/or getter and setter)
+ */
+public class BloodPressureReading {
+
+    private int sys;
+    private int dia;
+    private long timeStamp;
+
+    public void setSys(int sys) {
+        this.sys = sys;
+    }
+
+    public void setDia(int dia) {
+        this.dia = dia;
+    }
+
+    public long getTimeStamp() {
+        return timeStamp;
+    }
+
+    public int getSys() {
+        return sys;
+    }
+
+    public int getDia() {
+
+        return dia;
+    }
+
+    public BloodPressureReading(int sys, int dia){
+        this.sys = sys;
+        this.dia = dia;
+        this.timeStamp = System.currentTimeMillis();
+    }
+
+    public BloodPressureReading(){
+    }
+
+}
